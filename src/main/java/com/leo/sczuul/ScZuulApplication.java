12d4da3c4c08bb1d75a6490989c20024d565725e
wangly1004 <wangliyingwad@163.com>
2018-01-26 20:10:17 +0800

    1.zuul initial

---
+++ src/main/java/com/leo/sczuul/ScZuulApplication.java
@@ -0,0 +1,16 @@
+package com.leo.sczuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ScZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScZuulApplication.class, args);
+    }
+}
